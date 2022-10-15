f0773051ac16aafaadb53909a313beebf8660b4e
hada0127 <work@tarucy.net>
2022-10-15 21:53:55 +0900

    Apply .gitignore

--- .routify/instance.default.js
+++
@@ -1,8 +0,0 @@
-import { Router, createRouter } from '@roxi/routify'
-import routes from './routes.default.js'
-
-export const router = createRouter({routes})
-export { Router, routes }
-
-
-import '@roxi/routify/lib/buildtime/plugins/devHelper/runtime.js'
--- .routify/render.js
+++
@@ -1,7 +0,0 @@
-
-        import * as module from '../src/App.svelte'
-        import { renderModule } from '@roxi/routify/tools'
-
-        export const render = url => renderModule(module, url)
-
-import '@roxi/routify/lib/buildtime/plugins/devHelper/runtime.js'
