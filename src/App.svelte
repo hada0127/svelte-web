e01e5ca357bec438f4a17befe7551cea2f66f105
hada0127 <work@tarucy.net>
2022-12-15 06:35:50 +0900

    reset

--- src/App.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { Router } from "@roxi/routify";
-	import routes from "../.routify/routes.default.js";
-
-	import "@public/css/global.scss";
-</script>
-
-<Router routes={routes} />
